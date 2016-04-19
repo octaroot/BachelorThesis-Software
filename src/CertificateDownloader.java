2742025e80394c7e9107134059bad0ff74e24896
Martin Černáč <me@martincernac.cz>
2016-04-19 16:34:44 +0200

    Implement Certificate downloader

--- src/CertificateDownloader.java
+++ src/CertificateDownloader.java
@@ -1,10 +1,11 @@
-import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateEncodingException;
-import java.security.cert.X509Certificate;
+import java.security.cert.*;
 import java.security.interfaces.RSAPublicKey;
 
 /**
@@ -15,45 +16,45 @@ public class CertificateDownloader
 {
 	public static void main(String[] args) throws Exception
 	{
-		URL url = CertificateDownloader.class.getResource("keystore.jks");
+		URL keystorePath = CertificateDownloader.class.getResource("keystore.jks");
 
-		System.setProperty("javax.net.ssl.trustStore", url.getPath());
+		System.setProperty("javax.net.ssl.trustStore", keystorePath.getPath());
 		System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
 
-		//sni.velox.ch nebude s false fungovat
-		//fit.cvut.cz nebude s true fungovat
-		//System.setProperty("jsse.enableSNIExtension", "false");
 
+		String[] domainsToCheck = {
+				"https://wrong.host.badssl.com",
+		        "https://martincernac.cz",
+		        "https://toools.martincernac.cz",
+		        "https://cvut.cz",
+		        "https://fit.cvut.cz",
+		        "https://users.fit.cvut.cz"
+		};
 
-		//pro testovani se muze hodit https://badssl.com/
-		URL destinationURL = new URL("https://crypto.stackexchange.com/");
-		HttpsURLConnection conn = (HttpsURLConnection) destinationURL.openConnection();
-		try
+		for (String domain : domainsToCheck)
 		{
-			conn.connect();
-		}
-		catch (Exception ex)
-		{
-			//Zajima nas jen ta prvni vyjimka
-			while (ex.getCause() != null && !ex.getCause().equals(ex))
+			CertificateResponse response = processDomain(domain);
+			System.out.print(domain + ": ");
+			if (response.getException() != null)
 			{
-				ex = (Exception)ex.getCause();
+				if (response.getCertificate() != null)
+				{
+					System.out.println("[BAD] " + response.getException().getMessage());
+				}
+				else
+				{
+					System.out.println("[ERR] " + response.getException().getMessage());
+				}
 			}
-
-			//http://stackoverflow.com/questions/7615645/ssl-handshake-alert-unrecognized-name-error-since-upgrade-to-java-1-7-0
-			if (ex.getMessage().equals("handshake alert:  unrecognized_name"))
+			else
 			{
-				throw new RuntimeException("Badly configured SNI-supporting server. Recheck later with -Djsse.enableSNIExtension=false");
+				System.out.println("[OK!]");
 			}
-			throw ex;
 		}
-		Certificate[] certs = conn.getServerCertificates();
-		System.out.println("nb = " + certs.length);
 
-		if (certs.length > 0)
+		/*if (cert != null)
 		{
 			//zajima nas jen peer cert, chain jde mimo
-			Certificate cert = certs[0];
 			if (cert.getPublicKey() instanceof RSAPublicKey)
 			{
 				System.out.println("Modulus: " + ((RSAPublicKey) cert.getPublicKey()).getModulus());
@@ -65,23 +66,113 @@ public class CertificateDownloader
 				System.out.println("Not a RSA-based cert: " + cert.getPublicKey().getAlgorithm());
 			}
 
-			//vsechny certy jsou x509 certy ... https://bugzilla.mozilla.org/show_bug.cgi?id=290029
-			//ff jiny ani nepodporuje. simulujeme chovani ff (via truststore)
-			if (!(cert instanceof X509Certificate))
+			System.out.println("Signature Algo: " + ((X509Certificate) cert).getSigAlgName());
+			System.out.println("Issuer DN: " + ((X509Certificate) cert).getIssuerDN());
+			System.out.println("Subject DN: " + ((X509Certificate) cert).getSubjectDN());
+			System.out.println("Thumbprint: " + getThumbPrint((X509Certificate) cert));
+		}*/
+	}
+
+	/**
+	 * Process domain's certificate download. Attempt to download & verify the certificate and output the results
+	 *
+	 * @param domain The target domain
+	 * @return A structure with both the certificate (if downloaded) and the exception (if there was one)
+	 * @throws MalformedURLException
+	 */
+	public static CertificateResponse processDomain(String domain) throws MalformedURLException
+	{
+		URL domainName = new URL(domain);
+		Certificate cert;
+		try
+		{
+			//Attempt to download & validate the certificate
+			cert = downloadCertificate(domainName, true);
+			//Return it immediately, if successful
+			return new CertificateResponse((X509Certificate) cert);
+		}
+		catch (Exception ex)
+		{
+			//The certificate didn't validate, or there was another issue
+
+			//We are only interested in the original cause of failure
+			while (ex.getCause() != null && !ex.getCause().equals(ex))
 			{
-				System.err.println("Unknown certificate type: " + cert);
+				ex = (Exception) ex.getCause();
 			}
-			else
+
+			try
 			{
-				System.out.println("Signature Algo: " + ((X509Certificate) cert).getSigAlgName());
-				System.out.println("Issuer DN: " + ((X509Certificate) cert).getIssuerDN());
-				System.out.println("Subject DN: " + ((X509Certificate) cert).getSubjectDN());
-				System.out.println("Thumbprint: " + getThumbPrint((X509Certificate) cert));
+				//Attempt to at least download the certificate, without validating it at all
+				cert = downloadCertificate(domainName, false);
+				//If successful, return the downloaded certificate and the validation exception
+				return new CertificateResponse((X509Certificate) cert, ex);
+			}
+			catch (Exception ex2)
+			{
+				//If we can't even download the certificate with no checks, return the exception alone
+				return new CertificateResponse(ex);
 			}
 		}
 	}
-	public static String getThumbPrint(X509Certificate cert)
-			throws NoSuchAlgorithmException, CertificateEncodingException
+
+	/**
+	 * **
+	 * Opens a SSL connection to domain specified and performs SSL handshake.
+	 *
+	 * @param domain The target domain name
+	 * @return X509Certificate Peer certificate
+	 * @throws CertificateParsingException  When no certificate has been provided
+	 * @throws CertificateEncodingException When the certificate provided is not in standard X.509 format
+	 * @throws IOException                  In other cases (e.g. unable to open the connection, untrusted certificate, expired certificate, ...)
+	 */
+	public static X509Certificate downloadCertificate(URL domain,
+	                                                  boolean verifyCertificate) throws CertificateException, IOException, KeyManagementException, NoSuchAlgorithmException
+	{
+		HttpsURLConnection connection = (HttpsURLConnection) (verifyCertificate ? generateSafeConnection(domain) : generateUnsafeConnection(domain));
+
+		Certificate[] certificates;
+		//HttpsURLConnection provides full certificate validation, we only need to handle the exceptions
+		try
+		{
+			connection.connect();
+			certificates = connection.getServerCertificates();
+		}
+		finally
+		{
+			//In any case, we should always close the connection
+			connection.disconnect();
+		}
+
+		//We only care about the server's certificate, the (now verified) chain does not interest us anymore
+		if (certificates.length > 0)
+		{
+			if (!(certificates[0] instanceof X509Certificate))
+			{
+				//We don't support other formats than a standard X.509 certificate
+				//This is because we are mimicking the behavior of Mozilla Firefox 45
+				//which doesn't support other (e.g. OpenPGP based) keys.
+				throw new CertificateEncodingException("Server's certificate is not in X.509 format");
+			}
+
+			return (X509Certificate) certificates[0];
+		}
+		else
+		{
+			throw new CertificateParsingException("No certificate provided");
+		}
+
+	}
+
+	/**
+	 * Generates a certificate "Thumbprint". This is used as a unique identifier of any given certificate. The value is calculated as a SHA1 hash of the certificate in DER format.
+	 *
+	 * @param cert The certificate to generate a thumbprint for
+	 * @return Printable SHA1 hash in hexadecimal format
+	 * @throws NoSuchAlgorithmException
+	 * @throws CertificateEncodingException
+	 */
+	public static String getThumbPrint(X509Certificate cert) throws NoSuchAlgorithmException, CertificateEncodingException
 	{
 		MessageDigest md = MessageDigest.getInstance("SHA-1");
 		byte[] der = cert.getEncoded();
@@ -91,10 +182,15 @@ public class CertificateDownloader
 
 	}
 
-	public static String hexify (byte bytes[]) {
-
-		char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7',
-		                    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	/**
+	 * Helper function to create a printable (human readable) version of the byte-array specified
+	 *
+	 * @param bytes The byte array to be converted into a human-readable form
+	 * @return Human readable string of hex-digits, representing the specified byte-array
+	 */
+	public static String hexify(byte bytes[])
+	{
+		char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 		StringBuilder buf = new StringBuilder(bytes.length * 2);
 
@@ -105,5 +201,60 @@ public class CertificateDownloader
 		}
 
 		return buf.toString();
+	}
+
+	/**
+	 * Generates an unsafe HttpsURLConnection in such a way, that trusts all presented certificates.
+	 *
+	 * @param domain The target domain name
+	 * @return An instance of HttpsURLConnection with all validations and checks disabled
+	 * @throws NoSuchAlgorithmException
+	 * @throws KeyManagementException
+	 * @throws IOException
+	 */
+	public static HttpsURLConnection generateUnsafeConnection(URL domain) throws NoSuchAlgorithmException, KeyManagementException, IOException
+	{
+		SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init(null, new TrustManager[]{new X509TrustManager()
+		{
+			public java.security.cert.X509Certificate[] getAcceptedIssuers()
+			{
+				return null;
+			}
+
+			public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+			{
+
+			}
+
+			public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+			{
+			}
+		}}, null);
+
+		HttpsURLConnection connection = (HttpsURLConnection) domain.openConnection();
+
+		connection.setSSLSocketFactory(sc.getSocketFactory());
+		connection.setHostnameVerifier(new HostnameVerifier()
+		{
+			public boolean verify(String hostname, SSLSession session)
+			{
+				return true;
+			}
+		});
+
+		return connection;
+	}
+
+	/**
+	 * Generates a regular, safe HttpsURLConnection that validates certificates
+	 *
+	 * @param domain The target domain name
+	 * @return An instance of HttpsURLConnection with all validations and checks enabled
+	 * @throws IOException
+	 */
+	public static HttpsURLConnection generateSafeConnection(URL domain) throws IOException
+	{
+		return (HttpsURLConnection) domain.openConnection();
 	}
 }
---
+++ src/CertificateResponse.java
@@ -0,0 +1,49 @@
+import java.security.cert.X509Certificate;
+
+/**
+ * @author martin (cernama9@fit.cvut.cz)
+ * @since 19.4.16.
+ */
+public class CertificateResponse
+{
+	private X509Certificate certificate;
+	private Exception       exception;
+
+	CertificateResponse(X509Certificate certificate)
+	{
+		this.certificate = certificate;
+		this.exception = null;
+	}
+
+	CertificateResponse(X509Certificate certificate, Exception exception)
+	{
+		this.certificate = certificate;
+		this.exception = exception;
+	}
+
+	CertificateResponse(Exception exception)
+	{
+		this.exception = exception;
+		this.certificate = null;
+	}
+
+	public X509Certificate getCertificate()
+	{
+		return certificate;
+	}
+
+	public void setCertificate(X509Certificate certificate)
+	{
+		this.certificate = certificate;
+	}
+
+	public Exception getException()
+	{
+		return exception;
+	}
+
+	public void setException(Exception exception)
+	{
+		this.exception = exception;
+	}
+}
